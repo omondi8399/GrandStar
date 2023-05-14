79c7c3c37fac501188113505fa93545c1e1c129e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-14 17:14:51 +0300

    Updated Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,16 @@
 import Hero from "../components/Hero"
+import Projects from "../components/Projects"
 
 const Home = () => {
   return (
     <>
     <Hero />
+    <Projects />
+    <div className="flex justify-center items-center my-5">
+    <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white
+            font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Load more</button>
+
+    </div>
     </>
   )
 }
