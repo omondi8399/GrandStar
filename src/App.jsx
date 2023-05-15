ef449b0684be7ac2e3ac06519617a80d0f21c0e1
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-15 18:35:40 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Header from "./components/Header"
 import Home from "./views/Home"
 import Project from './views/Project'
+import { isWallectConnected } from './services/blockchain'
+import { useEffect } from 'react'
 
 const App = () => {
+  useEffect(async () => {
+    await isWallectConnected()
+    console.log('Blockchain Loaded')
+  }, [])
   return (
     <div className="min-h-screen relative">
       <Header />
