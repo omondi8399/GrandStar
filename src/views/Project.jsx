3bb6be288f0b8f0194e0c801c09b4806ee8ac703
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-15 12:56:50 +0300

    Updated Project view

--- src/views/Project.jsx
+++ src/views/Project.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ProjectDetails from '../components/ProjectDetails'
 import ProjectBackers from '../components/ProjectBackers'
+import UpdateProject from '../components/UpdateProject'
+import BackProject from '../components/BackProject'
+import DeleteProject from '../components/DeleteProject'
 
 const Project = () => {
   return (
     <>
     <ProjectDetails />
     <ProjectBackers />
+    <UpdateProject />
+    <BackProject />
+    <DeleteProject />
     </>
   )
 }
