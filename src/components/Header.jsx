7b9df89a5420f83facd1abe2216d1e01a9404207
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-15 18:36:19 +0300

    updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { TbBusinessplan } from "react-icons/tb"
 import { Link } from "react-router-dom"
+import { connectWallet } from "../services/blockchain"
+import { truncate, useGlobalState } from "../store"
 
 const Header = () => {
+  const [connectedAccount] = useGlobalState('connectedAccount')
   return (
     <header className='flex justify-between items-center p-5 bg-white
     shadow-lg fixed top-0 left-0 right-0'>
@@ -11,8 +14,15 @@ const Header = () => {
         </Link>
 
         <div className='flex space-x-2 justify-center'>
-            <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white
-            font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Connect Wallet</button>
+            {connectedAccount ? (
+              <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white
+              font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'
+              >{truncate(connectedAccount, 4, 4, 11)}</button>
+            ) : (
+              <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white
+            font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'
+            onClick={connectWallet}>Connect Wallet</button>
+            )}
         </div>
     </header>
   )
