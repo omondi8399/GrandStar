dde96a87f7018af27fdd6a4ad512c88857cd75b8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-15 18:36:57 +0300

    updated CreateProject component

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,13 +1,38 @@
 import { FaTimes } from "react-icons/fa"
 import { setGlobalState, useGlobalState } from "../store"
+import { useState } from "react"
 
 const CreateProject = () => {
     const [createModal] = useGlobalState('createModal')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [cost, setCost] = useState('')
+    const [date, setDate] = useState('')
+    const [imageURL, setImageURL] = useState('')
+
+    const toTimestamp = (dateStr) => {
+        const dateObj = Date.parse(dateStr)
+        return dateObj / 1000
+    } 
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!title || !description || !cost || !date || !imageURL) return
+
+        const params = {
+            title,
+            description,
+            cost,
+            expiresAt: toTimestamp(date),
+            imageURL
+        }
+        console.log(params)
+    }
   return (
     <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black
     bg-opacity-50 transform transition-tranform duration-300 ${createModal}`}>
         <div className="bg-white shadow-xl shadow-black rounded-xl w-11/12 md:w-2/5 h-7/12 p-6">
-            <form className="flex flex-col">
+            <form onSubmit={handleSubmit} className="flex flex-col">
                 <div className="flex justify-between items-center">
                     <p className="font-semibold">Add Project</p>
                     <button type="button" className="border-0 bg-transparent focus:outline-none"
@@ -18,7 +43,9 @@ const CreateProject = () => {
 
                 <div className="flex justify-center items-center mt-5">
                     <div className="rounded-xl overflow-hidden h-20 w-20">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQli1IRc2DFJf05FmE_DVMoGQbJnFiMmTIygQ&usqp=CAU" alt='project title'
+                    <img src={
+                        imageURL || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQli1IRc2DFJf05FmE_DVMoGQbJnFiMmTIygQ&usqp=CAU'
+                    } alt='project title'
             className=' h-full w-full object-cover cursor-pointer' 
             />
                     </div>
@@ -26,27 +53,32 @@ const CreateProject = () => {
 
                 <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
                     <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="text" name="title" placeholder="Title" required />
+                    focus: ring-0"  type="text" name="title" placeholder="Title"
+                    onChange={(e) => setTitle(e.target.value)} value={title} required />
                 </div>
 
                 <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
                     <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="number" step={0.01} min={0.01} name="amount" placeholder="Amount (ETH)" required />
+                    focus: ring-0"  type="number" step={0.01} min={0.01} name="cost" placeholder="cost (ETH)" 
+                    onChange={(e) => setCost(e.target.value)} value={cost}required />
                 </div>
 
                 <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
                     <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="date" name="date" placeholder="Expires" required />
+                    focus: ring-0"  type="date" name="date" placeholder="Expires" 
+                    onChange={(e) => setDate(e.target.value)} value={date}required />
                 </div>
 
                 <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
                     <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="url" name="imageURL" placeholder="Image URL" required />
+                    focus: ring-0"  type="url" name="imageURL" placeholder="Image URL" 
+                    onChange={(e) => setImageURL(e.target.value)} value={imageURL}required />
                 </div>
 
                 <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
                     <textarea className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="text" name="description" placeholder="Description" required ></textarea>
+                    focus: ring-0"  type="text" name="description" placeholder="Description"
+                    onChange={(e) => setDescription(e.target.value)} value={description}required ></textarea>
                 </div>
 
                 <button type='submit' className='mt-5 inline-block px-6 py-2.5 bg-green-600 text-white
