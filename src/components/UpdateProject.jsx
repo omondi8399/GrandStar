a07701dc272332be4b66929e3b4d143a2d20703a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-16 11:36:14 +0300

    updated UpdateProject component

--- src/components/UpdateProject.jsx
+++ src/components/UpdateProject.jsx
@@ -1,58 +1,155 @@
-import { FaTimes } from "react-icons/fa"
-import { setGlobalState, useGlobalState } from "../store"
+import { useState } from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import { updateProject } from '../services/blockchain'
+import { useGlobalState, setGlobalState } from '../store'
 
-const UpdateProject = () => {
-    const [updateModal] = useGlobalState('updateModal')
+const UpdateProject = ({ project }) => {
+  const [updateModal] = useGlobalState('updateModal')
+  const [title, setTitle] = useState(project?.title)
+  const [description, setDescription] = useState(project?.description)
+  const [date, setDate] = useState(project?.date)
+  const [imageURL, setImageURL] = useState(project?.imageURL)
+
+  const toTimestamp = (dateStr) => {
+    const dateObj = Date.parse(dateStr)
+    return dateObj / 1000
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!title || !description || !date || !imageURL) return
+
+    const params = {
+      id: project?.id,
+      title,
+      description,
+      expiresAt: toTimestamp(date),
+      imageURL,
+    }
+
+    await updateProject(params)
+    toast.success('Project updated successffully, will reflect in 30sec.')
+    onClose()
+  }
+
+  const onClose = () => {
+    setGlobalState('updateModal', 'scale-0')
+  }
+
   return (
-    <div className={`fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black
-    bg-opacity-50 transform transition-tranform duration-300 ${updateModal}`}>
-        <div className="bg-white shadow-xl shadow-black rounded-xl w-11/12 md:w-2/5 h-7/12 p-6">
-            <form className="flex flex-col">
-                <div className="flex justify-between items-center">
-                    <p className="font-semibold">Edit Project</p>
-                    <button type="button" className="border-0 bg-transparent focus:outline-none"
-                    onClick={() => setGlobalState('updateModal', 'scale-0')}>
-                        <FaTimes />
-                    </button>
-                </div>
+    <div
+      className={`fixed top-0 left-0 w-screen h-screen flex
+    items-center justify-center bg-black bg-opacity-50
+    transform transition-transform duration-300 ${updateModal}`}
+    >
+      <div
+        className="bg-white shadow-xl shadow-black
+        rounded-xl w-11/12 md:w-2/5 h-7/12 p-6"
+      >
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <p className="font-semibold">Edit Project</p>
+            <button
+              onClick={onClose}
+              type="button"
+              className="border-0 bg-transparent focus:outline-none"
+            >
+              <FaTimes />
+            </button>
+          </div>
 
-                <div className="flex justify-center items-center mt-5">
-                    <div className="rounded-xl overflow-hidden h-20 w-20">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQli1IRc2DFJf05FmE_DVMoGQbJnFiMmTIygQ&usqp=CAU" alt='project title'
-            className=' h-full w-full object-cover cursor-pointer' 
+          <div className="flex justify-center items-center mt-5">
+            <div className="rounded-xl overflow-hidden h-20 w-20">
+              <img
+                src={
+                  imageURL ||
+                  'https://media.wired.com/photos/5926e64caf95806129f50fde/master/pass/AnkiHP.jpg'
+                }
+                alt="project title"
+                className="h-full w-full object-cover cursor-pointer"
+              />
+            </div>
+          </div>
+
+          <div
+            className="flex justify-between items-center
+          bg-gray-300 rounded-xl mt-5"
+          >
+            <input
+              className="block w-full bg-transparent
+            border-0 text-sm text-slate-500 focus:outline-none
+            focus:ring-0"
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
             />
-                    </div>
-                </div>
+          </div>
 
-                <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
-                    <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="text" name="title" placeholder="Title" required />
-                </div>
+          <div
+            className="flex justify-between items-center
+          bg-gray-300 rounded-xl mt-5"
+          >
+            <input
+              className="block w-full bg-transparent
+            border-0 text-sm text-slate-500 focus:outline-none
+            focus:ring-0"
+              type="date"
+              name="date"
+              placeholder="Expires"
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              required
+            />
+          </div>
 
-                <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
-                    <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="number" step={0.01} min={0.01} name="amount" placeholder="Amount (ETH)" required />
-                </div>
+          <div
+            className="flex justify-between items-center
+          bg-gray-300 rounded-xl mt-5"
+          >
+            <input
+              className="block w-full bg-transparent
+            border-0 text-sm text-slate-500 focus:outline-none
+            focus:ring-0"
+              type="url"
+              name="imageURL"
+              placeholder="Image URL"
+              onChange={(e) => setImageURL(e.target.value)}
+              value={imageURL}
+              required
+            />
+          </div>
 
-                <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
-                    <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="date" name="date" placeholder="Expires" required />
-                </div>
+          <div
+            className="flex justify-between items-center
+          bg-gray-300 rounded-xl mt-5"
+          >
+            <textarea
+              className="block w-full bg-transparent
+            border-0 text-sm text-slate-500 focus:outline-none
+            focus:ring-0"
+              type="text"
+              name="description"
+              placeholder="Description"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+            ></textarea>
+          </div>
 
-                <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
-                    <input className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="url" name="imageURL" placeholder="Image URL" required />
-                </div>
-
-                <div className="flex justify-between items-center bg-gray-300 rounded-xl mt-5">
-                    <textarea className="block w-full bg-transparent border-0 text-sm text-slate-500 focus:outline-none
-                    focus: ring-0"  type="text" name="description" placeholder="Description" required ></textarea>
-                </div>
-
-                <button type='submit' className='mt-5 inline-block px-6 py-2.5 bg-gray-600 text-white
-                    font-medium text-md leading-tight rounded-full shadow-md hover:bg-gray-700'>Update Project</button>
-            </form>
-        </div>
+          <button
+            type="submit"
+            className="inline-block px-6 py-2.5 bg-green-600
+            text-white font-medium text-md leading-tight
+            rounded-full shadow-md hover:bg-green-700 mt-5"
+          >
+            Update Project
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
