30bb129ae0aae08645f5f403daecbf6626ebeca8
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-16 10:12:37 +0300

    updated Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,23 @@
-import AddButton from "../components/AddButton"
-import CreateProject from "../components/CreateProject"
-import Hero from "../components/Hero"
-import Projects from "../components/Projects"
+import { useEffect } from 'react'
+import AddButton from '../components/AddButton'
+import CreateProject from '../components/CreateProject'
+import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import { loadProjects } from '../services/blockchain'
+import { useGlobalState } from '../store'
 
 const Home = () => {
+  const [projects] = useGlobalState('projects')
+
+  useEffect(async () => {
+    await loadProjects()
+  }, [])
   return (
     <>
-    <Hero />
-    <Projects />
-    <div className="flex justify-center items-center my-5">
-    <button type='button' className='inline-block px-6 py-2.5 bg-green-600 text-white
-            font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Load more</button>
-
-    </div>
-    <CreateProject />
-    <AddButton />
+      <Hero />
+      <Projects projects={projects} />
+      <CreateProject />
+      <AddButton />
     </>
   )
 }
